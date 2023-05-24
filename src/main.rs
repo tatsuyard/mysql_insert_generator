52f1dcca50212d0d77f4e502872a607359e8bab2
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2023-05-25 08:30:07 +0900

    display schema info

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,44 @@ use std::io::Write;
 fn main() {
     dotenv().ok(); // .env ファイルをロード
 
-    let database_url = env::var("DATABASE_URL_CORE").expect("DATABASE_URL must be set");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     let opts = mysql::Opts::from_url(&database_url).unwrap();
     let pool = Pool::new(opts).unwrap();
 
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
-        panic!("Usage: cargo run <table_name> <column1> [<column2>...]");
+        panic!("Usage: cargo run <command> <table_name> <column1> [<column2>...]");
     }
-    let table_name = &args[1];
-    let columns: Vec<&str> = args[2..].iter().map(AsRef::as_ref).collect();
+    let command = &args[1];
+    let table_name = &args[2];
+    let columns: Vec<&str> = args[3..].iter().map(AsRef::as_ref).collect();
 
-    generate_insert_statements(pool, table_name, columns);
+    match command.as_str() {
+        "schema" => print_schema(pool, table_name),
+        "insert" => generate_insert_statements(pool, table_name, columns),
+        _ => panic!("Unknown command: {}", command),
+    }
+}
+
+fn print_schema(pool: Pool, table_name: &str) {
+    let mut conn = pool.get_conn().unwrap();
+
+    let schema_query = format!("SHOW COLUMNS FROM {}", table_name);
+    let result = conn
+        .query_map(&schema_query, |row: Row| {
+            let values: Vec<Value> = row.unwrap();
+            values
+                .iter()
+                .map(|value| value_to_string(value))
+                .collect::<Vec<String>>()
+        })
+        .unwrap();
+
+    for row_values in result {
+        let schema_info = row_values.join(" | ");
+        println!("{}", schema_info);
+    }
 }
 
 fn generate_insert_statements(pool: Pool, table_name: &str, columns: Vec<&str>) {
